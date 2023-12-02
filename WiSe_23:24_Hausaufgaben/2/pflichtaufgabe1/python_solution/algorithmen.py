f74619fb04296f980544e493c95623fe07cdce2b
egeguenes <senugege@gmail.com>
2023-12-02 10:30:56 +0100

    mehhh

--- WiSe_23:24_Hausaufgaben/2/pflichtaufgabe1/python_solution/algorithmen.py
+++ WiSe_23:24_Hausaufgaben/2/pflichtaufgabe1/python_solution/algorithmen.py
@@ -2,26 +2,24 @@ def isSorted(arr):
 	return arr == sorted(arr)
 
 def sort(arr):
-	for i in range(len(arr)-1 , 1 , -1):
-		for j in range(0 , i-1):
+	for i in range(len(arr)-1 , 0 , -1):
+		for j in range(0 , i):
 			if arr[j] > arr[j+1]:
 				temp = arr[j]
 				arr[j] = arr[j+1]
 				arr[j+1] = temp
 	return arr
 
-def binarySearch(arr , n):
-	a = 0
-	b = len(arr)
-	while a != 0:
-		m = (a+b)//2
-		if arr[m] == n:
-			return True
-		else:
-			if n < arr[m]:
-				b = m
-			elif n > arr[m]:
-				a = m + 1
-			else:
-				pass
-	return False
+def binary_search(arr, x):
+    low = 0
+    high = len(arr) - 1
+    mid = 0
+    while low <= high:
+        mid = (high + low) // 2
+        if arr[mid] < x:
+            low = mid + 1
+        elif arr[mid] > x:
+            high = mid - 1
+        else:
+            return True
+    return False
--- WiSe_23:24_Hausaufgaben/2/pflichtaufgabe1/python_solution/main.py
+++ WiSe_23:24_Hausaufgaben/2/pflichtaufgabe1/python_solution/main.py
@@ -1,13 +1,9 @@
-import algorithmen
+from algorithmen import isSorted, sort, binary_search
 
-my_array = [1,23,4,5,6,3,42,1,43,4]
+my_array = [1, 23, 4, 5, 6, 3, 42, 7, 43]
 
 print(my_array)
-
-print(algorithmen.isSorted(my_array))
-
-print(algorithmen.sort(my_array))
-
-print(algorithmen.binarySearch(my_array , 100))
-
-print(algorithmen.binarySearch(my_array , 43))
+print(isSorted(my_array))
+print(sort(my_array))
+print(binary_search(my_array, 100))
+print(binary_search(my_array, 43))
